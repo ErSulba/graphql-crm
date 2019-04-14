c215339df7c5329f907fe61950193be7c075f7e0
Alejandro Sulbaran <ersulba@gmail.com>
2019-04-14 11:33:28 -0400

    [client] spinner added

---
+++ client/src/components/widgets/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className='preloader'>
+      <div className='cssload-speeding-wheel' />
+    </div>
+  );
+};
+
+export default Spinner;
