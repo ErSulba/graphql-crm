fd618daefd6ca1fb401c949c0b7b18a3e339d7dd
Alejandro Sulbaran <ersulba@gmail.com>
2019-08-07 19:09:52 -0400

    format

--- client/src/components/Form/CustomInput.js
+++ client/src/components/Form/CustomInput.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
 
-const customInput = ({ field, form: { touched, errors }, ...props }) => (
+const CustomInput = ({ field, form: { touched, errors }, ...props }) => (
   <div>
-    <Form.Control isInvalid={!!(touched[field.name] && errors[field.name])} {...field} {...props} />
+    <Form.Control
+      isInvalid={!!(touched[field.name] && errors[field.name])}
+      {...field}
+      {...props}
+    />
+
     {touched[field.name] && errors[field.name] && (
-      <Form.Control.Feedback type='invalid'>{errors[field.name]}</Form.Control.Feedback>
+      <Form.Control.Feedback type='invalid'>
+        {errors[field.name]}
+      </Form.Control.Feedback>
     )}
   </div>
 )
 
-export default customInput
+export default CustomInput
