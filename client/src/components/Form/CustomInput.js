1e9b598f5f3fb078ba01e1f42fe5ff85ddf0707e
Alejandro Sulbaran <ersulba@gmail.com>
2019-08-09 14:40:43 -0400

    [client] refactor to render options for select

--- client/src/components/Form/CustomInput.js
+++ client/src/components/Form/CustomInput.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
 
-const CustomInput = ({ field, form: { touched, errors }, ...props }) => (
+const CustomInput = ({
+  field,
+  form: { touched, errors },
+  children,
+  ...props
+}) => (
   <div>
     <Form.Control
       isInvalid={!!(touched[field.name] && errors[field.name])}
       {...field}
       {...props}
-    />
+    >
+      {children}
+    </Form.Control>
 
     {touched[field.name] && errors[field.name] && (
       <Form.Control.Feedback type='invalid'>
